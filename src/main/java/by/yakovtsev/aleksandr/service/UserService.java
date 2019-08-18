c603db9371c8108dc79834a0e54d73eb57dc5cf3
An4or <anchor_gomel@tut.by>
2019-08-18 15:03:32 +0300

    added UserService

--- src/main/java/by/yakovtsev/aleksandr/service/UserService.java
+++ src/main/java/by/yakovtsev/aleksandr/service/UserService.java
@@ -1,4 +1,39 @@
 package by.yakovtsev.aleksandr.service;
 
+import by.yakovtsev.aleksandr.dao.UserDao;
+import by.yakovtsev.aleksandr.dao.UserDaoImpl;
+import by.yakovtsev.aleksandr.model.Auto;
+import by.yakovtsev.aleksandr.model.User;
+
+import java.util.List;
+
 public class UserService {
+
+    private UserDao userDao = new UserDaoImpl();
+
+    public UserService(){}
+
+    public User findUser(int id) {
+        return userDao.findById(id);
+    }
+
+    public void saveUser(User user) {
+        userDao.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userDao.delete(user);
+    }
+
+    public void updateUser(User user) {
+        userDao.update(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userDao.findAll();
+    }
+
+    public Auto findAutoById(int id) {
+        return userDao.findAutoById(id);
+    }
 }
